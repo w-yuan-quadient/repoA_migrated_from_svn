627893ded25465da160b148c9595b40ff914b9ad
w-yuan-quaident <w.yuan@quadient.com>
2026-07-13 23:54:31 +0000

    Add a comment line on branches/feature-1
    git-svn-id: svn://localhost/repoA/branches/feature-1@8 dba8a85c-9419-485b-81d7-4a719143d2fa

--- VectorAdd/VectorAdd.cpp
+++ VectorAdd/VectorAdd.cpp
@@ -2,6 +2,7 @@
 
 void vector_add(const double* a, const double* b, double* out, int size)
 {
+	// this line is added in branches/feature-1
     for (int i = 0; i < size; ++i)
     {
         out[i] = a[i] + b[i];
